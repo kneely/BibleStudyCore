7153c4f4deb6f9d793ccfcd4894ae988c77dfbe5
Kevin <kevin.r.neely@outlook.com>
2018-06-19 13:09:10 -0400

    Add project files.

---
+++ BibleStudyCore/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BibleStudyCore.Data;
+using BibleStudyCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BibleStudyCore.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private string Id { get; }
+
+        public UserController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+    //    public async Task<IEnumerable<User>> Index()
+    //    {
+    //        //return await _dbContext.User.FromSql("dbo.GetVerseById @Id", Id).ToListAsync();
+    //    }
+    }
+}
---
+++ BibleStudyCore/Data/BibleDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BibleStudyCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BibleStudyCore.Data
+{
+    public class BibleDbContext   : DbContext
+    {
+        public BibleDbContext(DbContextOptions<BibleDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<User> User { get; set; }
+    }
+}
