be9590083401388019ab40ef7b419cb159341bfa
Kevin Neely <kevin.r.neely@outlook.com>
2018-08-22 12:58:56 -0400

    Ready for production

--- BibleStudyCore/Program.cs
+++ BibleStudyCore/Program.cs
@@ -19,6 +19,7 @@ namespace BibleStudyCore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:5001")
                 .UseStartup<Startup>();
     }
 }
